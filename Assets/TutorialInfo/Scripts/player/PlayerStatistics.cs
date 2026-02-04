a9c664d81e439559e3ca889d97e8a49a63df83f1
SachaAerts <sacha50cl@hotmail.com>
2026-02-04 18:35:19 +0100

    correction wall texture + project done

--- Assets/TutorialInfo/Scripts/player/PlayerStatistics.cs
+++ Assets/TutorialInfo/Scripts/player/PlayerStatistics.cs
@@ -1,6 +1,6 @@
 public record PlayerStatistics
 {
-    public float Range { get; } = 4f;
+    public float Range { get; } = 10f;
     public float Speed { get; } = 10f;
     public int Damages { get; } = 50;
     public float FireRate { get; } = 0.2f;
